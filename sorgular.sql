af74d15550a056aa73f9e8a0f183a3b0ee8cc68d
Kandahar Bozkurt ÖZTÜRK <kandahar.ozturk@sngict.com>
2024-07-26 03:54:58 +0300

    Yeni sorgular eklendi.

--- sorgular.sql
+++ sorgular.sql
@@ -75,4 +75,71 @@ delete from izler
 where kullaniciid=1 and filmid=0;
 
 
+--PROFİL EKRANI
+
+--Kullanıcının kullanıcı adı bilgisinin getirilmesi.(kullaniciid=1)
+
+select kullaniciadi
+from kullanici
+where kullaniciid=1;
+
+--Kullanıcının izlediği bütün filmlerin getirilmesi.
+
+select f.ad 
+from film f
+natural join izler
+natural join kullanici k
+where k.kullaniciid =1;
+
+--Kullanıcının beğendiği bütün filmlerin getirilmesi.
+
+select f.ad
+from film f 
+natural join begenir b 
+natural join kullanici k 
+where k.kullaniciid =1;
+
+--Kullanıcının en çok tercih ettiği 3 film türünün getirilmesi.
+
+select t.genreadi
+from terciheder t 
+natural join kullanici k 
+where k.kullaniciid =1;
+
+--EXTRA
+
+--Bir genreye ait filmlerin popülerliğe göre azalan sırada getirilmesi.
+
+select f.ad
+from film f
+natural join aittir a 
+natural join genre g 
+where g.genreadi ='genreName'
+order by f.populerlik desc ;
+
+--Bir oyuncunun oynadığı bütün filmlerin getirilmesi.
+
+select f.ad
+from film f 
+natural join oyuncular o 
+where o.oyuncu ='starName';
+
+--Bir yönetmenin yönettiği bütün filmlerin getirilmesi.
+
+
+
+--Belirli bir puanın üzerindeki filmlerin getirilmesi.
+
+select f.ad
+from film f 
+where populerlik >1;
+
+--Kullanıcı tercih ettiği film türlerini eklemesi.
+
+insert into terciheder
+values(1,'genreName');
+
+
+
+
 
